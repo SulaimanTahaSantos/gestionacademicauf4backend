36dc769c79560ab4b8122a7933457d19e14898fd
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2025-05-17 12:59:41 +0200

    Modificacion de cors

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,13 @@ return [
     |
     */
 
-    'paths' => ['*'],
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:5173',"http://localhost:3000"],
-
+    'allowed_origins' => ['http://localhost:3000'], // Añade aquí tu dominio frontend
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true,
+    'supports_credentials' => true, // Esto es importante
 
 ];
