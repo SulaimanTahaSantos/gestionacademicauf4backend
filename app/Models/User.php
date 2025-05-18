b705af6410e41af4254414b89d1f0ce2fc4817cd
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2025-05-18 17:20:49 +0200

    cambio en el controladorv4

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use App\Models\Grupo;
 use App\Models\Clase;
 use Tymon\JWTAuth\Contracts\JWTSubject;
@@ -13,7 +14,7 @@ use App\Models\Perfil;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
