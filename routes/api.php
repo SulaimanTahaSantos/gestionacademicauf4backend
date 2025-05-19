145dc2c8cf2cdd85763f85f66acf1e05adb676fa
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2025-05-19 18:13:37 +0200

    quitandolo del middleware adminv2

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,7 @@ Route::middleware([IsUserAuth::class])->group(function(){
 Route::middleware([IsAdmin::class])->group(function(){
     Route::get('/users/{id}', [UserController::class, 'show']);
     // Route::post('/users', [UserController::class, 'store']);
-    Route::put('/users/{id}', [UserController::class, 'update']);
-    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    // Route::put('/users/{id}', [UserController::class, 'update']);
+    // Route::delete('/users/{id}', [UserController::class, 'destroy']);
 });
 ?>
