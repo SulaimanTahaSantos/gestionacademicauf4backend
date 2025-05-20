859f09c956fe9914b9b8393884c8a73d63ac9cd4
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2025-05-20 19:23:44 +0200

    agregando cors session y sanctum

--- config/cors.php
+++ config/cors.php
@@ -1,33 +1,11 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Aquí puedes configurar tus ajustes para cross-origin resource sharing
-    | o "CORS". Esto determina qué operaciones de cross-origin pueden ejecutarse
-    | en navegadores web. Usualmente, las APIs de navegador no permiten cross-origin
-    | a menos que se implemente esta característica.
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
+    'allowed_origins' => ['http://localhost:3000', 'https://tu-dominio-frontend.com'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'], // En producción, limita a tu dominio del frontend
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => ['X-XSRF-TOKEN', 'Content-Type', 'X-Requested-With', 'Accept', 'Origin', 'Referer'],
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true, // Importante para enviar cookies de autenticación
-
+    'supports_credentials' => true,
 ];
