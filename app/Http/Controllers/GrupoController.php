18fa2da1c1086e26a0156babf9c54df525959df9
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2025-05-21 16:13:11 +0200

    ppp

---
+++ app/Http/Controllers/GrupoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Grupo;
+use App\Models\Cursar;
+use App\Models\Modulo;
+use App\Models\User;
+
+
+
+
+class GrupoController extends Controller
+{
+    
+    
+}
