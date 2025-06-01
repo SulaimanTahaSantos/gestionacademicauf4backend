c7a58ed01bf363f5260f63cea47c3fca071299ab
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2025-06-01 22:45:07 +0200

    Solo he puesto mi web del deploy de frontend para que funcionen las peticiones

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://gestionacademicauf4backend-production.up.railway.app'],
+    'allowed_origins' => ['http://localhost:3000', 'https://gestionacademicauf4backend-production.up.railway.app', 'https://gestionacademicauf4frontend.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
